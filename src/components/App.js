2e02f0ebd6a4566b86bfafe5f2bc39af94b919ac
BalaChandraAkula <balachandraakula698@gmail.com>
2023-06-04 10:25:44 +0530

    submitting code-Sun Jun 04 2023 10:25:42 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,45 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react'
+import '../styles/App.css';
+import Button from './Button';
 
+const App = () => {
 
-const App = ()=>{
-    let [inputvalue,setInputValue] = useState("");
-    function renderInput(e){
-        let numberValue = e.target;
-        setInputValue(inputvalue+numberValue.innerText);
-
+    const data = ["C", "/", "*", "-", "7", "8", "9", "+", "4", "5", "6", "1", "2", "3", "=", "0", ".", ""]
+    let [exp, setExp] = useState("");
+    
+    function handleClick(id) {
+        if (id === "=") {
+            try {
+                console.log(eval(exp));
+                if (eval(exp)!="undefined") {
+                    setExp(eval(exp))
+                }
+            }catch (error) {
+                setExp("Error");
+              }
+           
+           
+        } else if (id=="C") {
+            setExp("");
+        } else {
+            setExp(() => exp + id);
+        }    
     }
-    function calculateInput(){
-        try{
-            let ans = eval(inputvalue);
-            setInputValue(ans);
 
-        }
-        catch(err){
-            setInputValue("Invalid Input");
-        }
-        
-        
-    }
-    function clearInput(){
-        setInputValue("");
-    }
-    return(
-        <div className='calc-div'>
-        <h1>Calculator</h1>
-        <input type='text' value={inputvalue} readOnly></input>
-        <div className="btn-div">
-        <button onClick={clearInput}>C</button>
-        <button onClick={renderInput}>/</button>
-        <button onClick={renderInput}>*</button>
-        <button onClick={renderInput}>-</button>
-        <button onClick={renderInput}>7</button>
-        <button onClick={renderInput}>8</button>
-        <button onClick={renderInput}>9</button>
-        <button onClick={renderInput}>+</button>
-        <button onClick={renderInput}>4</button>
-        <button onClick={renderInput}>5</button>
-        <button onClick={renderInput}>6</button>
-        <button onClick={renderInput}>1</button>
-        <button onClick={renderInput}>2</button>
-        <button onClick={renderInput}>3</button>
-        <button onClick={calculateInput}>=</button>
-        <button onClick={renderInput}>0</button>
-        <button onClick={renderInput}>.</button>
-
+    console.log(exp);
+    return (
+        <div className='container Calculator'>
+            <div className='expression' id="calci">{exp}</div>
+            <div className="button-container">
+                {data.map((key) => (
+                    <Button key={key} id={key}  handleClick={handleClick}></Button>
+                ))}
+            </div>
         </div>
-        
-        </div>
+
 
     )
-    
 }
+
+
 export default App;
